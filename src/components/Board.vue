64bb0a43f859649885c23221a9d469acd7a0cb96
tomnkgw <69185254+tomnkgw@users.noreply.github.com>
2020-09-01 18:56:42 +0900

    initial commit.
    特になし

---
+++ src/components/Board.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <header>
+      My Trello
+    </header>
+    <main>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
+      <draggable :list="lists"
+            @end="movingList"
+            class="list-index">
+        <list v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :cards="item.cards"
+              :listIndex="index"
+              @change="movingCard"
+        />
+        <list-add/>
+      </draggable>
+    </main>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+import List from './List'
+import ListAdd from './ListAdd'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    ListAdd,
+    List,
+    draggable
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    }
+  },
+  methods: {
+    movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    }
+  }
+}
+</script>
